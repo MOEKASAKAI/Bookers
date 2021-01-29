74c3923239286d56f07aeab0e7450a9eaddd9fb8
MOEKASAKAI <wpi.floves2@gmail.com>
2021-01-29 07:21:29 +0000

    [Update] 3-8 delete機能

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
 end
