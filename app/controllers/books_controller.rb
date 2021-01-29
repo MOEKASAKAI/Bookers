77251478b305870242ad7868a8d8917ea1f8af4b
MOEKASAKAI <wpi.floves2@gmail.com>
2021-01-29 05:46:11 +0000

    [Update] 3-8 詳細画面機能紐付け

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/index'
+    redirect_to book_path(book.id)
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'books#top'
   get 'index' => 'books#index'
-  get 'show' => 'books#show'
   get 'top' => 'books#top'
   get 'books/:id' => 'books#show', as: 'book'
   post 'books' => 'books#create'
